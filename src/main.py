4456e996c7c0539f6bac651ec14e334bb31068e4
camfietz <cfietz13@icloud.com>
2024-03-12 09:04:10 -0700

    imported all csv files into a single dictionary
    see comments on how to access dictionary

--- src/main.py
+++ src/main.py
@@ -2,12 +2,6 @@ import csv
 import os
 import pprint
 
-# create player class
-
-# extract each row of each input file into player class
-
-# add each player to array displaying important stats
-
 class Player:
     def __init__(self, id, season, name, team, games_played, goals, assists, points, powerplay_goals, shorthanded_goals, special_goals, shots, hits, blocks):
         self.id = int(id)
@@ -29,44 +23,55 @@ class Player:
         return str(self.name) + " " + str(self.season) + " " + str(self.games_played) + "GP " + str(self.goals) + "G " + str(self.assists) + "A " + str(self.points) + "P " + str(self.powerplay_goals) + "PPG " + str(self.shorthanded_goals) + "SHG " + str(self.special_goals) + "SG " + str(self.shots) + "SOG " + str(self.hits) + "H " + str(self.blocks) + "B"
     
     __repr__ = __str__
-    
-season_dictionary = {}
 
-current_dir = os.path.dirname(__file__)
-file_path = os.path.join(current_dir, '../res/moneypuck/moneypuck_2023-24.csv')
+dataset_dictionary = {}
 
-with open(file_path) as file: 
-    reader = csv.reader(file)
-    headers = next(reader) 
-    for row in reader:
-        # create dictionary entry
-        id = int(row[0])
-        if id not in season_dictionary:
-            season_dictionary[id] = Player(id, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+for season in range(2008, 2023):  
+    season = str(season) + '-' + str((season + 1))[-2:]
+    season_dictionary = {}
+
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, '../res/moneypuck/moneypuck_' + season + '.csv')
+
+    with open(file_path, 'r') as file: 
+        reader = csv.reader(file)
+        headers = next(reader) 
+        for row in reader:
+            # create dictionary entry
+            id = int(row[0])
+            if id not in season_dictionary:
+                season_dictionary[id] = Player(id, '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+            
+            # all situations
+            if (row[5] == 'all'):
+                season_dictionary[id].season = int(float(row[1]))
+                season_dictionary[id].name = str(row[2])
+                season_dictionary[id].team = str(row[3])
+                season_dictionary[id].games_played = int(float(row[6]))
+                season_dictionary[id].goals = int(float(row[34]))
+                season_dictionary[id].assists = int(float(row[27])+float(row[28]))
+                season_dictionary[id].points = int(float(row[33]))
+                season_dictionary[id].shots = int(float(row[29]))
+                season_dictionary[id].hits = int(float(row[46]))
+                season_dictionary[id].blocks = int(float(row[83]))
+
+            # powerplay (5on4 only)
+            elif (row[5] == '5on4'):
+                season_dictionary[id].powerplay_goals = int(float(row[34]))
+
+            # shorthanded (4on5 only)
+            elif (row[5] == '4on5'):
+                season_dictionary[id].shorthanded_goals = int(float(row[34]))
+            
+            # all other situations (5on3, 6on5, 3on3, etc.)
+            elif (row[5] == 'other'):
+                season_dictionary[id].special_goals = int(float(row[34]))
         
-        # all situations
-        if (row[5] == 'all'):
-            season_dictionary[id].season = int(float(row[1]))
-            season_dictionary[id].name = str(row[2])
-            season_dictionary[id].team = str(row[3])
-            season_dictionary[id].games_played = int(float(row[6]))
-            season_dictionary[id].goals = int(float(row[34]))
-            season_dictionary[id].assists = int(float(row[27])+float(row[28]))
-            season_dictionary[id].points = int(float(row[33]))
-            season_dictionary[id].shots = int(float(row[29]))
-            season_dictionary[id].hits = int(float(row[46]))
-            season_dictionary[id].blocks = int(float(row[83]))
+        dataset_dictionary[season] = season_dictionary
+        #pprint.pprint(season_dictionary)
 
-        # powerplay (5on4 only)
-        elif (row[5] == '5on4'):
-            season_dictionary[id].powerplay_goals = int(float(row[34]))
+#pprint.pprint(dataset_dictionary)
 
-        # shorthanded (4on5 only)
-        elif (row[5] == '4on5'):
-            season_dictionary[id].shorthanded_goals = int(float(row[34]))
-        
-        # all other situations (5on3, 6on5, 3on3, etc.)
-        elif (row[5] == 'other'):
-            season_dictionary[id].special_goals = int(float(row[34]))
-
-    pprint.pprint(season_dictionary)
+# to access a player for a specific season (season = str, id = int):
+# dataset_dictionary['season'][id]
+pprint.pprint(dataset_dictionary['2022-23'][8478402])

103800374f0b96ff82bee8500cf7cec8c763243b
ericyu02 <ericsentyu@gmail.com>
2024-03-13 11:16:19 -0700

    Take command line arguments

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
+import argparse
+import csv
 import os
-from dataset import dataset_dictionary
 
-print(dataset_dictionary['2022-23'][8478402])
+from dataset import dataset_dictionary
+from predict_score import predict
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("season", help="select season to show i.e. 2022-23")
+parser.add_argument("id", help="id of player", type=int)
+args = parser.parse_args()
+
+season = int(args.season.split("-")[0])
+
+print(predict(season, args.id))
